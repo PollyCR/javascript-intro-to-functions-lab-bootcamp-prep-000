fe25cd2261b656f32beffeb9c30d46243a6c27cc
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-05 17:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHiToGrandma(string) {
-  return string.toUpperCase('hello')
+function sayHiToGrandma(hello) {
+  return string('I CAN\'T HEAR YOU!')
 }
